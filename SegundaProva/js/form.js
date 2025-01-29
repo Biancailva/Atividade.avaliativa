144fae3e3a16d85da1ac2f98d48d73051ca5b1f5
Natalia Bianca <145494932+Biancailva@users.noreply.github.com>
2025-01-28 21:58:54 -0300

    Add files via upload

---
+++ SegundaProva/js/form.js
@@ -0,0 +1,12 @@
+prompt('Digite seu nome:')
+function alterarTexto(){
+    var titulo = document.getElementById('nome')
+    titulo.innerText = 'Clicou'
+}
+function inserirNome(prompt){
+    var nome = document.write('nome').prompt
+    nome.innerText = (prompt)
+}
+var h2 = document.getElementById('nome')
+h2.addEventListener('click' , inserirNome)
+
